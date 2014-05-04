2280d38c00fd5169b4a171e31d812558b6152229
Cory Taylor <cory.c.taylor@gmail.com>
2014-05-04 12:48:15 -0400

    reset

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
-var ReleaseList = require('./routes/releaseList');
-var releaseList = new ReleaseList(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.configure(function () {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
-    app.use(favicon());
-    app.use(logger());
-    app.use(bodyParser());
-    app.use(express.methodOverride());
-    app.use(cookieParser());
-    app.use(express.session());
-    app.use(everyauth.middleware(app));
-    app.use(app.router);
-    app.use(require('less-middleware')({ src: __dirname + '/public' }));
-    app.use(express.static(path.join(__dirname, 'public')));
-});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -35,8 +30,6 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
-
-//app.get('/releases', releaseList.showReleases.bind(releaseList));
 
 /// error handlers
 
